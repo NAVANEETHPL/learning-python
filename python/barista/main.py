e6505b126e41afca473f163a0ff25a76b336bb6a
NAVANEETHPL <85995846+NAVANEETHPL@users.noreply.github.com>
2022-05-08 13:05:01 +0530

    computer barista :)

---
+++ python/barista/main.py
@@ -0,0 +1,49 @@
+name = input("what is your name?\n")
+
+
+if name == "ben":
+  evil_status = input("are you evil ben?\n")
+  
+  if evil_status == "yes":
+   print("you're not welcome here EVIL BEN!!. get out ")
+   exit()
+  else:
+   print("oh,so you're one of the good bens. come in.")  
+else:
+ print("hello " + name + ", thank you for coming,have a geat day.\n")
+   
+ menu = "\n black coffee  \n espresso  \n latte  \n cappucino\n frappuccino \n"
+
+order = input("what would you like from our menu today? Here is what we serving." + menu )
+
+
+
+quantity = input("how many " + order +  "s would you like?\n")
+
+
+price = 8
+
+if order == "frappuccino":
+ price = 13
+
+if order == "black coffee":
+ price = 3
+
+
+
+if order == "latte":
+ price = 9
+
+
+
+if order == "cappuccino":
+ price = 4
+
+total = price * int(quantity)
+
+print("Thank you. Your total is:$ " + str(total))
+print("Sounds good " + name +",We will have your " + quantity + " " + order + " in a moment.")
+
+welcome = input("thank you\n")
+if welcome == "welcome":
+  print("it's our pleasure")
